ead6eb44cc92dcc814cef2d9fc65896ad876a484
marzavec <admin@marzavec.com>
2022-05-06 09:33:35 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# install-scripts
-Scripts to install hack.chat onto a linux or windows system
+# Install Scripts
+
+The scripts contained in this repo can be used to easily install hack.chat on a linux or windows system. The lines below will assist in downloading and running the install scripts.
+
+---
+
+## Ubuntu or Debian
+
+```
+curl -fsSL https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | sudo -E bash -
+```
+
+or
+
+```
+wget -qO- https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | sudo -E bash -
+```
+
+## Windows
+
+```
+Coming soon ;]
+```
---
+++ hcInstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Discussion, issues and change requests at:
+#   https://github.com/hack-chat/install-scripts
+#
+# Script to install hack.chat repo onto a Debian or Ubuntu system.
+#
+# Run as root or insert `sudo -E` before `bash`:
+#
+# curl -sL https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | bash -
+#   or
+# wget -qO- https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | bash -
+#
+
+cd ~
+
+sudo apt update
+sudo apt install -y build-essential git
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+node -v
+npm -v
+git --version
+
+git clone https://github.com/hack-chat/main.git hackchat
+cd hackchat
+npm install
+npm run start
