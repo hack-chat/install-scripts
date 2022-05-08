f513405b07ea69857916650e1c0e5ed8c8ab0bc1
carrot <carrot.moncher@gmail.com>
2022-05-08 20:11:13 +0800

    Some changes
    - Running as root isn't necessary, since sudo is called inside where needed
    - Package build-essential isn't necessary (from what I've tested)
    - Script will now exit if any command finishes with a non-zero exit code

--- hcInstall.sh
+++ hcInstall.sh
@@ -1,29 +1,35 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+#
 # Discussion, issues and change requests at:
 #   https://github.com/hack-chat/install-scripts
 #
 # Script to install hack.chat repo onto a Debian or Ubuntu system.
-#
-# Run as root or insert `sudo -E` before `bash`:
 #
 # curl -sL https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | bash -
 #   or
 # wget -qO- https://raw.githubusercontent.com/hack-chat/install-scripts/main/hcInstall.sh | bash -
 #
 
-cd ~
+WORK_DIR="$HOME"
+NODE_SETUP_SOURCE="https://deb.nodesource.com/setup_18.x"
 
-sudo apt update
-sudo apt install -y build-essential git
+if [[ $EUID = 0 ]]; then
+    echo "This script should not be run as root."
+    exit 1
+fi
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+cd $WORK_DIR &&
 
-node -v
-npm -v
-git --version
+sudo apt update &&
+sudo apt install -y git curl &&
 
-git clone https://github.com/hack-chat/main.git hackchat
-cd hackchat
+curl -fsSL $NODE_SETUP_SOURCE | sudo -E bash - &&
+sudo apt-get install -y nodejs &&
+
+echo -n "node: " && node -v &&
+echo -n "npm: " && npm -v &&
+echo -n "git: " && git --version &&
+
+git clone https://github.com/hack-chat/main.git hackchat &&
+cd hackchat &&
 npm install
